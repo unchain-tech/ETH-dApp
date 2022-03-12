20f412deb4a7abcaa696a271d5319c1a084d8b8f
Alejandro Cremades <ale@alecrem.com>
2022-03-12 17:11:10 +0900

    First step

--- src/App.js
+++ src/App.js
@@ -13,11 +13,74 @@ const App = () => {
   /**
   * デプロイされたコントラクトのアドレスを保持する変数を作成
   */
-  const contractAddress = "0x8EdaBe4013480F30e0d9093a9D7aFAE282271027";
+  const contractAddress = "0x384B51056d43Cfa004aa3Bf39d09FeE44f47674F";
    /**
   * ABIの内容を参照する変数を作成
   */
   const contractABI = abi.abi;
+
+  const getAllWaves = async () => {
+    const { ethereum } = window;
+  
+    try {
+      if (ethereum) {
+      const provider = new ethers.providers.Web3Provider(ethereum);
+      const signer = provider.getSigner();
+      const wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      /* コントラクトからgetAllWavesメソッドを呼び出す */
+      const waves = await wavePortalContract.getAllWaves();
+      /* UIに必要なのは、アドレス、タイムスタンプ、メッセージだけなので、以下のように設定 */
+      const wavesCleaned = waves.map(wave => {
+        return {
+        address: wave.waver,
+        timestamp: new Date(wave.timestamp * 1000),
+        message: wave.message,
+        };
+      });
+  
+      /* React Stateにデータを格納する */
+      setAllWaves(wavesCleaned);
+      } else {
+      console.log("Ethereum object doesn't exist!");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  /**
+   * `emit`されたイベントに反応する
+   */
+  useEffect(() => {
+    let wavePortalContract;
+  
+    const onNewWave = (from, timestamp, message) => {
+      console.log("NewWave", from, timestamp, message);
+      setAllWaves(prevState => [
+      ...prevState,
+      {
+        address: from,
+        timestamp: new Date(timestamp * 1000),
+        message: message,
+      },
+      ]);
+    };
+  
+    /* NewWaveイベントがコントラクトから発信されたときに、情報をを受け取ります */
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+  
+      wavePortalContract = new ethers.Contract(contractAddress, contractABI, signer);
+      wavePortalContract.on("NewWave", onNewWave);
+    }
+    /*メモリリークを防ぐために、NewWaveのイベントを解除します*/
+    return () => {
+      if (wavePortalContract) {
+      wavePortalContract.off("NewWave", onNewWave);
+      }
+    };
+  }, []);
 
   /*
   * window.ethereumにアクセスできることを確認します。
