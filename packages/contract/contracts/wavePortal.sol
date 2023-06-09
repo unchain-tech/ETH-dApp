4d4dd2ad63f0397360726c3875b2bdbc7e7bb3c1
sorag <gkthn20@gmail.com>
2023-06-09 23:13:34 +0900

    230609

--- packages/contract/contracts/wavePortal.sol
+++ packages/contract/contracts/wavePortal.sol
@@ -4,8 +4,18 @@ pragma solidity ^0.8.18;
 
 import "hardhat/console.sol";
 
-contrat WavePortal {
+contract WavePortal {
+    uint256 totalWaves;
     constructor(){
-        console.log("Here is my first smart contract!")
+        console.log("Here is my first smart contract!");
+    }
+      function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
     }
 }
