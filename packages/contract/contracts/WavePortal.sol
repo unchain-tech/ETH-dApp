b84265c4acc846c678204291f74b4b9002f34e7d
toryoshi <44675166+hidaytours@users.noreply.github.com>
2023-11-02 17:18:47 +0900

    add Neevent: wWave at WavePortal.sol

--- packages/contract/contracts/WavePortal.sol
+++ packages/contract/contracts/WavePortal.sol
@@ -8,13 +8,30 @@ contract WavePortal {
 
     uint256 private _totalWaves;
 
+    // NewWaveイベント?の作成
+    event NewWave(address indexed from, uint256 timestamp, string message);
+    
+    struct Wave {
+        address waver;
+        string message;
+        uint256 timestamp;
+    }
+    
+    Wave[] private _waves;
+
     constructor() {
-        console.log("Here is my first smart contract!");
+        console.log("WavePortal - Smart Contract!");
     }
 
-    function wave() public {
+    function wave(string memory _message) public {
         _totalWaves += 1;
-        console.log("%s has waved!", msg.sender);
+        console.log("%s waved w/ message %s", msg.sender, _message);
+        _waves.push(Wave(msg.sender, _message, block.timestamp));
+        emit NewWave(msg.sender, block.timestamp, _message);
+    }
+
+    function getAllWaves() public view returns (Wave[] memory) {
+        return _waves;
     }
 
     function getTotalWaves() public view returns (uint256) {
