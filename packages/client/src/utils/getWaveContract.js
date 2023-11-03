592fb4cecf6eb65a0655b44420206ce04993723b
toryoshi <44675166+hidaytours@users.noreply.github.com>
2023-11-03 22:31:01 +0900

    deploy contract

--- packages/client/src/utils/getWaveContract.js
+++ packages/client/src/utils/getWaveContract.js
@@ -2,7 +2,7 @@ import { ethers } from "ethers";
 
 import abi from "./WavePortal.json"
 
-const contractAddress = "0x3a87963C4C3e0c9C18c770e3d2D9397876eA7f5A";
+const contractAddress = "0x4f5D963b57F6c6e44d9623F72a4AB808d742CCf8";
 const contractABI = abi.abi;
 
 export const getWavePortalContract = (isReadOnly) => {
